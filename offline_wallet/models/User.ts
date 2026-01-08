5aa37fe424e16311e70876c1c5be37fa053539f7
Sujan Thapa <sujanthapa4001@gmail.com>
2026-01-09 01:09:22 +0530

    commit

--- offline_wallet/models/User.ts
+++ offline_wallet/models/User.ts
@@ -1,4 +1,4 @@
-import { IUser } from '../../types/offline_wallet_types/user.interface'
+import { IUser } from '../types/user.interface'
 import { Wallet } from "./wallet"
 
 export class User implements IUser {
